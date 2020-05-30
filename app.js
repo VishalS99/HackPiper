bad58146d7bf5f5b1e57137fa9c8f15336e8843d
Vishal Saranathan <s-vishal1999@hotmail.com>
2020-05-30 14:55:48 +0530

    Fixd auth

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
+var authRouter = require("./routes/loginRoute");
+
 // var usersRouter = require("./routes/users");
 
 var app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -19,7 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/home", indexRouter);
+app.use("/login", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/server.js
+++ routes/server.js
@@ -1,103 +1,100 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-// const { shopRoute } = require('./shopRoute');
-// const { cartRoute } = require('./cartRoute');
-// const { buyRoute } = require('./buyRoute');
-// const { loginRoute } = require('./loginRoute');
-// const { billingRoute } = require('./billingRoute');
-// const { devStatusRoute } = require('./devStatusRoute');
-// const { signupRoute } = require('./signupRoute');
-const cookieParser = require('cookie-parser');
-const mysql = require('mysql');
-const passport = require('passport');
-const LocalStrategy   = require('passport-local').Strategy;
-const app = express();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// // const { shopRoute } = require('./shopRoute');
+// // const { cartRoute } = require('./cartRoute');
+// // const { buyRoute } = require('./buyRoute');
+// // const { loginRoute } = require('./loginRoute');
+// // const { billingRoute } = require('./billingRoute');
+// // const { devStatusRoute } = require('./devStatusRoute');
+// // const { signupRoute } = require('./signupRoute');
+// const cookieParser = require('cookie-parser');
+// const mysql = require('mysql');
+// const passport = require('passport');
+// const LocalStrategy   = require('passport-local').Strategy;
+// const app = express();
 
-app.set('view engine', 'ejs');
-app.use(cors());
-app.use(bodyParser.json());
-app.use(cookieParser());
+// app.set('view engine', 'ejs');
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(cookieParser());
 
-// app.use("/api/shop", shopRoute);
-// app.use("/api/cart", cartRoute);
-// app.use("/api/buy", buyRoute);
-// app.use("/api/login", loginRoute);
-// app.use("/api/signup", signupRoute);
-// app.use("/api/dev/status", devStatusRoute);
-// app.use("/api/billing", billingRoute);
+// // app.use("/api/shop", shopRoute);
+// // app.use("/api/cart", cartRoute);
+// // app.use("/api/buy", buyRoute);
+// // app.use("/api/login", loginRoute);
+// // app.use("/api/signup", signupRoute);
+// // app.use("/api/dev/status", devStatusRoute);
+// // app.use("/api/billing", billingRoute);
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'piper'
-})
-connection.connect()
-connection.query('use piper');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'piper'
+// })
+// connection.connect()
+// connection.query('use piper');
 
+// passport.use(new LocalStrategy({
+//         usernameField : 'user',
+//         passwordField : 'pass',
+//     },
+//     function(user, pass, done) {
+//         connection.query("SELECT pass FROM piper_auth WHERE user = '" + user + "'", (err,rows) => {
+// 			if (err)	{
+// 				console.log("errrr");
+//                 return done(err);
+// 			}
+// 			if (!rows.length) {
+// 				console.log("len 0");
+//                 return done(null, false);
+//             }
 
-passport.use(new LocalStrategy({
-        usernameField : 'user',
-        passwordField : 'pass',
-    },
-    function(user, pass, done) {
-        connection.query("SELECT pass FROM piper_auth WHERE user = '" + user + "'", (err,rows) => {
-			if (err)	{
-				console.log("errrr");
-                return done(err);
-			}
-			if (!rows.length) {
-				console.log("len 0");
-                return done(null, false); 
-            } 
-			
-			if (!( rows[0].password == password))
-				console.log("wrong pass");
-                return done(null, false); 
-			console.log("hey");	
-            // all is well, return successful user
-            return done(null, rows[0]);			
-	});
-	
-}));
+// 			if (!( rows[0].password == password))
+// 				console.log("wrong pass");
+//                 return done(null, false);
+// 			console.log("hey");
+//             // all is well, return successful user
+//             return done(null, rows[0]);
+// 	});
 
-app.get("/", (req, res) => {
-	console.log("hello");
-	res.cookie('name', 'express').send('cookie set');
-	connection.query("SELECT * FROM piper_auth WHERE name = \"hariom\" ", (err,rows) => {
-		console.log(err, rows)
-			if (err)	{
-				console.log("errrr");
-				// res.send("lamo");
-                // return done(err);
-			}
-			else if (rows.length === 0) {
-				console.log("len 0");
-				// res.send("lamo");
-            } 
-			
-			else (!( rows[0].pass == pass))
-				console.log("wrong pass");
-			console.log("hey");	
-		});
-});
+// }));
 
+// app.get("/", (req, res) => {
+// 	console.log("hello");
+// 	res.cookie('name', 'express').send('cookie set');
+// 	connection.query("SELECT * FROM piper_auth WHERE name = \"hariom\" ", (err,rows) => {
+// 		console.log(err, rows)
+// 			if (err)	{
+// 				console.log("errrr");
+// 				// res.send("lamo");
+//                 // return done(err);
+// 			}
+// 			else if (rows.length === 0) {
+// 				console.log("len 0");
+// 				// res.send("lamo");
+//             }
 
+// 			else (!( rows[0].pass == pass))
+// 				console.log("wrong pass");
+// 			console.log("hey");
+// 		});
+// });
 
-app.post('/login', 
-	passport.authenticate('local', { failureRedirect: '/login' }),
- 	function(req, res) {
-		console.log("hellooooo");
-    	res.redirect('/');
-	}
-);
+// app.post('/login',
+// 	passport.authenticate('local', { failureRedirect: '/login' }),
+//  	function(req, res) {
+// 		console.log("hellooooo");
+//     	res.redirect('/');
+// 	}
+// );
 
-app.get('/login', (req, res) => {
-	console.log("ouch");
-	return res.status(200).json({hey: "hey"});
-})
+// app.get('/login', (req, res) => {
+// 	console.log("ouch");
+// 	return res.status(200).json({hey: "hey"});
+// })
 
-app.listen(8000, () => {
-    console.log(`Server listening on port 8000`);
-});
+// app.listen(8000, () => {
+//     console.log(`Server listening on port 8000`);
+// });
