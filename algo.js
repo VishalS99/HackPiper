7dd0eb239002d122df3463c0c80b42048a364a84
prateek1337 <prateeksahu1337@gmail.com>
2020-05-30 16:04:51 +0530

    updated queries

--- algo.js
+++ algo.js
@@ -1,26 +1,22 @@
-function algo(){
+function algo(id,distance,zone,category){
     const threshold=20;
     var zone_priority={
         red:{
             medical:7,
             food:3,
-            others:1,
+            other:1,
         },
         orange:{
            medical:5,
           food:4,
-             others:2	
+             other:2	
         },
         normal:{
             medical:3,
             food:3,
-            others:2
+            other:2
         }
     }
-    
-    distance=300//some function to get nearest warehouse distance from billing address.
-    zone="red"//get zone from billing address
-    category="medical"//get item category
     distace_priority=0;
     
     if(distance<100){
@@ -33,11 +29,12 @@ function algo(){
         distace_priority=10
     }
     final_priority=distance_priority*zone_priority[zone][category];
-    alert(final_priority)
+    console.log()
     if(final_priority<threshold){
         alert("Cannot deliver");
     }
     if(final_priority>threshold){
+        //add to priority queue.
         alert("Order booked");
     }
 }
